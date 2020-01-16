60f74736b6999769f872c2829185e68eaf23a720
Josh Hunt <josh@trtr.co>
2020-01-16 15:38:42 +0000

    fix

--- api/csv.js
+++ api/csv.js
@@ -6,8 +6,6 @@ const CSV_SHEET =
   "https://docs.google.com/spreadsheets/d/e/2PACX-1vQEPSQuQfkgWOK30e9dPLs1x2UkZYhJfdpOeCuxrffz-jSA50ZK8QFmtaTXuSu9KmTq2sYSdy1fSvuw/pub?output=csv";
 
 export default (req, res) => {
-  res.json({ name: "John", email: "john@example.com" });
-
   console.log("request at", new Date());
 
   axios.get(CSV_SHEET).then(res => {
