9c6e2d9bb0dae7690b0d13cb6c75eacc69f22b4a
Josh Hunt <josh@trtr.co>
2020-01-16 15:41:55 +0000

    fix it properly

--- api/csv.js
+++ api/csv.js
@@ -5,10 +5,10 @@ var axios = require("axios");
 const CSV_SHEET =
   "https://docs.google.com/spreadsheets/d/e/2PACX-1vQEPSQuQfkgWOK30e9dPLs1x2UkZYhJfdpOeCuxrffz-jSA50ZK8QFmtaTXuSu9KmTq2sYSdy1fSvuw/pub?output=csv";
 
-export default (req, res) => {
+export default (req, httpres) => {
   console.log("request at", new Date());
 
   axios.get(CSV_SHEET).then(res => {
-    res.send(res.data);
+    httpres.send(res.data);
   });
 };
